166440c829e3ba5a9ab3a315af251ffdd644ef58
hamidreza <hamidrezaramzani80@gmail.com>
2025-06-05 16:27:37 +0330

    add nvim config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move left" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move right" })
+
